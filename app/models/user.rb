ab789a42e4b3ec1cc9dca06b4bf54439da83dcd8
keisuke.kato <kgulliverk@hotmail.com>
2020-05-02 17:28:25 +0900

    メソッド名変更 destory&update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  before_destroy :destroy_action
-  before_update :update_action
+  before_destroy :reject_destroy_admin
+  before_update :reject_update_admin
 
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
@@ -14,14 +14,14 @@ class User < ApplicationRecord
 
   private
 
-  def destroy_action
+  def reject_destroy_admin
     if User.where(admin: true).count == 1
       user = User.where(admin: true)
       throw :abort if user[0] == self
     end
   end
 
-  def update_action
+  def reject_update_admin
     if User.where(admin: true).count == 1 && admin == false
       user = User.where(admin: true)
       if user[0] == self
